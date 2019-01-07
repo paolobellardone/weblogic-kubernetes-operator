808c75fe223a8a83929e984738ec71d31e53bcd9
Mark Nelson <mark.x.nelson@oracle.com>
2019-01-07 10:18:42 -0500

    updates

--- operator-web-ui/src/shared/Rest.js
+++ operator-web-ui/src/shared/Rest.js
@@ -1,4 +1,4 @@
 // Copyright 2019, Oracle Corporation and/or its affiliates. All rights reserved.
 
-export const REST_ADDRESS = 'https://internal-weblogic-operator-svc:8082/'
+export const REST_ADDRESS = 'https://129.146.70.82:31001/'
 export const REST_AUTH_HEADER = 'Bearer ' + process.env.K8S_TOKEN
